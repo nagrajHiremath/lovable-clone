a98cff3e7a84ab39f77bc6c5fdc7c1d31c8d0930
Nagraj <nagrajh303@gmail.com>
2026-05-27 23:36:48 +0530

    Spring init

---
+++ src/test/java/com/codingshuttle/project/lovable_clone/LovableCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.project.lovable_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovableCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
